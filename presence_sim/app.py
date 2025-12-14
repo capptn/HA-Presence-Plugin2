3405312a36228b33296f007a85040b0f75a8c2ba
Capptn.com Fabian Gdanietz <info@capptn.com>
2025-12-14 14:00:08 +0000

    update

--- presence_sim/app.py
+++ presence_sim/app.py
@@ -1,1 +1,11 @@
-print("Presence Simulation Add-on started")
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("presence_sim")
+
+logger.info("Presence Simulation Add-on started")
+
+# Keep container alive
+while True:
+    time.sleep(60)
